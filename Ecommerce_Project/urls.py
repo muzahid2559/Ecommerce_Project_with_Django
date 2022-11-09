d0ea8ed54ab8a276401e11fb4d8dc399d335f452
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-11-09 11:13:57 +0600

    Project and App Setup-2

--- Ecommerce_Project/urls.py
+++ Ecommerce_Project/urls.py
@@ -7,6 +7,8 @@ from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('App_Shop.urls')),
+    path('account/', include('App_Login.urls')),
 ]
 
 
