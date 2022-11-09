1f7073065c83c2f02fe73411633bcb476b5ba1ab
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-11-09 22:08:02 +0600

    Messages Setup

--- App_Login/views.py
+++ App_Login/views.py
@@ -13,6 +13,8 @@ from django.contrib.auth import login, logout, authenticate
 from App_Login.models import Profile
 from App_Login.forms import ProfileForm, SignUpForm
 
+# Messages
+from django.contrib import messages
 
 # Create your views here.
 
@@ -22,6 +24,7 @@ def sign_up(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Account Created Successfully!")
             return HttpResponseRedirect(reverse('App_Login:login'))
     return render(request, 'App_Login/sign_up.html', context={'form':form})
 
@@ -43,4 +46,17 @@ def login_user(request):
 @login_required
 def logout_user(request):
     logout(request)
+    messages.warning(request, "You are logged out!!")
     return HttpResponse('Logged Out')
+
+@login_required
+def user_profile(request):
+    profile = Profile.objects.get(user=request.user)
+    form = ProfileForm(instance=profile)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Change Saved!!")
+            form = ProfileForm(instance=profile)
+    return render(request, 'App_Login/change_profile.html', context={'form':form})
--- Ecommerce_Project/settings.py
+++ Ecommerce_Project/settings.py
@@ -129,4 +129,4 @@ MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
 
-LOGIN_URL = '/'
+LOGIN_URL = '/account/login/'
